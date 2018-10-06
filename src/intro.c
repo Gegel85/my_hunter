8c395facffc82331165f662f426bbdc24188aa18
Gegel85 <Gegel85@laposte.net>
2018-10-06 12:38:29 +0200

    Fixed a bug in the intro

--- src/intro.c
+++ src/intro.c
@@ -7,6 +7,7 @@
 
 #include "structs.h"
 #include "macros.h"
+#include <stdio.h>
 #include <SFML/Graphics.h>
 
 void	set_dog_rect_int(game_t *game, int i)
@@ -14,6 +15,8 @@ void	set_dog_rect_int(game_t *game, int i)
 	int		h = game->dog.animation % 4;
 	sfVector2f	pos = {0, 552};
 
+	if (h > 2)
+		h = 2;
 	game->dog.speed += ABS(game->dog.speed) * 0.1 + 0.75;
 	game->var = 0.5;
 	game->dog.x += 2 * h;
@@ -21,7 +24,7 @@ void	set_dog_rect_int(game_t *game, int i)
 		game->dog.y += game->dog.speed;
 	if (game->dog.y > 525 && game->dog.speed > 0)
 		game->menu = 1;
-	if (h == 2 || h == 3) {
+	if (h == 2) {
 		game->sprites[6].rect.left = game->background * 256;
 		sfSprite_setPosition(game->sprites[6].sprite, pos);
 	}
@@ -40,6 +43,7 @@ void	set_dog_sprite(game_t *game)
 		game->dog.animation = 0;
 	if (game->intro > 159) {
 		i = 2;
+		game->dog.animation = (game->intro - 159) / 15;
 		set_dog_rect_int(game, i);
 	} else {
 		game->dog.x += 2;
@@ -54,18 +58,15 @@ void	set_dog_sprite(game_t *game)
 void	update_dog(game_t *game)
 {
 	sfTime		time;
-	int		h = 0;
 	float		sec;
 
 	time = sfClock_getElapsedTime(game->clock);
 	sec = sfTime_asSeconds(time);
 	set_dog_sprite(game);
 	if (sec > game->var) {
-		h = game->dog.animation % 5;
-		if (h == 4)
-			game->dog.animation -= 4;
-		else if (game->intro < 190)
+		if (game->intro < 190)
 			game->dog.animation++;
+		game->dog.animation %= 4;
 		sfClock_restart(game->clock);
 	}
 	game->intro++;
